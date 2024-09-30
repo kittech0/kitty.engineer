b18bbb7973d4f2077df91191fa993461d15036dc
kittech0 <kittech@skiff.com>
2024-09-30 13:03:45 +0200

    Added blog site

---
+++ script/blog.js
@@ -0,0 +1,15 @@
+// typeText(String,String,Int)
+function typeText(text, elementId, delay) {
+    let textShifted = text.substr(1);
+    let i = 0;
+    for (let letter of textShifted) {
+        setTimeout(function () {
+            document.getElementById(elementId).textContent += letter;
+        }, i * delay);
+        i++;
+    }
+}
+
+window.onload = () => {
+    typeText('. Kittech\'s Blog 🐈', 'title-text', 100);
+};
