64e42004a406be250613347369b7607535d5d4ea
kittech0 <kittech7@gmail.com>
2024-11-16 10:43:47 +0100

    removed dot

--- script/lib.js
+++ script/lib.js
@@ -1,7 +1,7 @@
 // typeText(String,Int)
 export function typeText(elementId, delay) {
     let textShifted = document.getElementById(elementId).textContent;
-    document.getElementById(elementId).textContent = ".";
+    document.getElementById(elementId).textContent = "";
     let i = 0;
     for (let letter of textShifted) {
         setTimeout(function () {
