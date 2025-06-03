7eb0393e9b321180262fcb41d4aca035b44e9854
kittech <kittech7@gmail.com>
2025-06-03 21:00:34 +0200

    end of work

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import {defineConfig} from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [],
+    output: 'static',
+    compressHTML: true,
+    site: "https://kitty.engineer",
+    prefetch: true,
+    experimental: {
+        fonts: [{
+            provider: "local",
+            name: "Monaspace Neon",
+            cssVariable: "--font-neon",
+            variants: [{
+                style: "normal",
+                src: ["./src/font/MonaspaceNeon-Regular.woff2",],
+            },],
+        }, {
+            provider: "local",
+            name: "Monaspace Radon",
+            cssVariable: "--font-radon",
+            variants: [{
+                weight: 700,
+                style: "normal",
+                src: ["./src/font/MonaspaceRadon-Regular.woff2",],
+            },],
+        },],
+    },
+});
